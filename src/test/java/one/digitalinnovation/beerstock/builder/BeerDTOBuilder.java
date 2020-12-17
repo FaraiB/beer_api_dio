b9871ff03b44c1a5ad27651e53851aefbdfb30b9
Farai <fabvu8@gmail.com>
2020-12-17 00:01:20 -0300

    First commit

--- src/test/java/one/digitalinnovation/beerstock/builder/BeerDTOBuilder.java
+++ src/test/java/one/digitalinnovation/beerstock/builder/BeerDTOBuilder.java
@@ -15,7 +15,7 @@ public class BeerDTOBuilder {
     private Long id = 1L;
 
     @Builder.Default
-    private String name = "Brahma";
+    private String name = "Franziskaner";
 
     @Builder.Default
     private String brand = "Ambev";
@@ -27,7 +27,7 @@ public class BeerDTOBuilder {
     private int quantity = 10;
 
     @Builder.Default
-    private BeerType type = BeerType.LAGER;
+    private BeerType type = BeerType.WEISS;
 
     public BeerDTO toBeerDTO() {
         return new BeerDTO(id,
